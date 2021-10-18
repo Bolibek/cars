cb4621032e5ebd6ba1f4fba39282cc640b337125
Bolibek <fayzullayevbolibek@gmail.com>
2021-10-19 00:11:46 +0900

    message

---
+++ js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find module '@babel-preset-env/babel-preset'\nRequire stack:\n- C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js\n- C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\files\\index.js\n- C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\index.js\n- C:\\xampp\\htdocs\\end\\node_modules\\babel-loader\\lib\\index.js\n- C:\\xampp\\htdocs\\end\\node_modules\\loader-runner\\lib\\loadLoader.js\n- C:\\xampp\\htdocs\\end\\node_modules\\loader-runner\\lib\\LoaderRunner.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack\\lib\\NormalModuleFactory.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack\\lib\\Compiler.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack\\lib\\webpack.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack\\lib\\index.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack-cli\\lib\\webpack-cli.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack-cli\\lib\\bootstrap.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack-cli\\bin\\cli.js\n- C:\\xampp\\htdocs\\end\\node_modules\\webpack\\bin\\webpack.js\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:902:15)\n    at resolve (C:\\xampp\\htdocs\\end\\node_modules\\v8-compile-cache\\v8-compile-cache.js:164:23)\n    at resolveStandardizedName (C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:111:7)\n    at resolvePreset (C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:59:10)\n    at loadPreset (C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:78:20)\n    at loadPreset.next (<anonymous>)\n    at createDescriptor (C:\\xampp\\htdocs\\end\\node_modules\\@babel\\core\\lib\\config\\config-descriptors.js:187:16)\n    at createDescriptor.next (<anonymous>)\n    at step (C:\\xampp\\htdocs\\end\\node_modules\\gensync\\index.js:261:32)\n    at evaluateAsync (C:\\xampp\\htdocs\\end\\node_modules\\gensync\\index.js:291:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/script.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
